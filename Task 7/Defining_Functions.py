a415762d8c5fa0b3ea55c183ac4766d4bb1be397
Draygo22 <52995134+Draygo22@users.noreply.github.com>
2019-09-09 15:41:17 +0200

    Add files via upload

---
+++ Task 7/Defining_Functions.py
@@ -0,0 +1,11 @@
+def adding(n1,n2,n3):
+  sum_of_all = n1 + n2 + n3
+  return sum_of_all
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+#sum_of_all = num1 + num2 + num3
+
+print(adding(num1, num2, num3))
---
+++ Task 7/baby.py
@@ -0,0 +1,9 @@
+age = int(input("Enter Age : "))                 
+
+if age>=18:                                     
+        status="Congrats, You're old enough"    
+		
+else:
+    status="You're too young!"
+
+print(status)                                  
